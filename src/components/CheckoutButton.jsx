d3eea9628d073e4a461cd95b4a9219565da2bf13
luiz.ricardo <luiz.ricardo@groupsoftware.com.br>
2024-10-01 14:54:25 -0300

    projeto

---
+++ src/components/CheckoutButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const CheckoutButton = ({cartItems, setCartItems}) => {
+
+    const navigate = useNavigate();
+
+    const handleCheckout = () => {
+        if (cartItems.length > 0){
+            toast.success("Compra finalizada com sucesso!");
+
+            navigate("/thank-you", { state : { cartItems } });
+
+            setCartItems([]);
+        } else {
+            toast.error("Seu carrinho está vazio");
+        }
+    };
+
+    return (
+        <button onClick={handleCheckout}>Finalizar compra</button>
+    )
+}
+
+export default CheckoutButton
